90e4e16a494107813437a7692309cdda6a454c04
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-12-11 08:33:52 +1000

    Install and configure Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import preact from '@astrojs/preact';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   // base: 'ironwill79.github.io',
-  integrations: [preact()],
+  integrations: [preact(), tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    applyBaseStyles: false,
+  })],
   site: 'https://ironwill79.github.io',
 });
