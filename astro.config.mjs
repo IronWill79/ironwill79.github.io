bd4daf7898b6cb6195a17f137821b2d395ad8a43
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-12-11 07:54:48 +1000

    Add GitHub Pages deployment action and configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
+  base: 'ironwill79.github.io',
+  integrations: [preact()],
   site: 'https://ironwill79.github.io',
-  integrations: [preact()]
 });
