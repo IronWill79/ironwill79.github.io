49cccd90a90b86c03c21edca853f0692d58d9a1c
IronWill79 <1820392+IronWill79@users.noreply.github.com>
2024-12-10 10:25:04 +1000

    Add Preact for Greeting component and add dark mode functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ironwill79.github.io'
+  site: 'https://ironwill79.github.io',
+  integrations: [preact()]
 });
